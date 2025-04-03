ed3a32835dd738c9e10dfbede264021d05866280
chowlong <adrianeroa081202@gmail.com>
2025-04-03 13:43:35 +0800

    feat(business): splash screen and app icon

--- business-client/app/index.tsx
+++ business-client/app/index.tsx
@@ -1,30 +1,5 @@
 import { Redirect } from "expo-router";
-import { useEffect, useState } from "react";
-import { Image, Text, View } from "react-native";
 
 export default function Main() {
-    const [loading, setLoading] = useState<boolean>(true);
-
-    useEffect(() => {
-        setTimeout(() => setLoading(false), 5000);
-    }, []);
-
-    if (loading) {
-        return (
-            <View className="h-screen w-screen flex items-center justify-center">
-                <Image
-                    source={require("~/assets/images/trabago-logo.png")}
-                    className="h-28 w-28"
-                />
-                <View className="flex flex-row gap-[4px]">
-                    <Text className="text-lg tracking-wider">For</Text>
-                    <Text className="text-lg text-primary font-bold tracking-wider">
-                        Business
-                    </Text>
-                </View>
-            </View>
-        );
-    }
-
     return <Redirect href="/home" />;
 }
