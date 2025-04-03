0b5d722dbd49ae7cba88f0eb3119bfd660420eec
chowlong <adrianeroa081202@gmail.com>
2025-04-03 13:28:24 +0800

    feat(client): splash screen and app icon

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,24 +1,5 @@
 import { Redirect } from "expo-router";
-import { useEffect, useState } from "react";
-import { Image, View } from "react-native";
 
 export default function Main() {
-    const [loading, setLoading] = useState<boolean>(true);
-
-    useEffect(() => {
-        setTimeout(() => setLoading(false), 5000);
-    }, []);
-
-    if (loading) {
-        return (
-            <View className="h-screen w-screen flex items-center justify-center">
-                <Image 
-                    source={require("~/assets/images/trabago-logo.png")}
-                    className="h-28 w-28"
-                />
-            </View>
-        );
-    }
-
     return <Redirect href="/home" />;
 }
